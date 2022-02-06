de517ed5c23ba7fc4e08dd5aa85042eb15bd45d3
xandyblackout <xandinho1984@gmail.com>
2022-02-06 18:42:33 -0300

    new

---
+++ interavel.py
@@ -0,0 +1,14 @@
+import sys
+import time
+
+def gera():
+    r = []
+    for n in range(100):
+        r.append(n)
+        time.sleep(0.1)
+    return r
+
+g = gera()
+
+# for v in g:
+#    print(v)
