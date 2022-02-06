11f868e87ca66d7308ab2f00d543ce39956cdd6f
xandyblackout <xandinho1984@gmail.com>
2022-02-06 20:31:15 -0300

    new

---
+++ app.py
@@ -0,0 +1,8 @@
+carrinho = []
+carrinho.append(('Produto 1', 30.50))
+carrinho.append(('Produto 2', 20))
+carrinho.append(('Produto 3', 50))
+
+total = sum([float(y) for x, y in carrinho])
+print(carrinho)
+print(total)
